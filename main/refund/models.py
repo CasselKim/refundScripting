6747888d11bf56433371dbe2aa5f9c13de695a2a
CasselKim <seongr39@gmail.com>
2023-01-14 16:31:12 +0900

    migrate Master Scheme

--- main/refund/models.py
+++ main/refund/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Master(models.Model) : 
+    business_id = models.CharField(max_length=11)
+    balance = models.IntegerField(default=0)
+    
 # Create your models here.
